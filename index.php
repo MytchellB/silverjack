967c53c2309b32461e2f95cee15b345c85d6065d
Mytchell Beaton <mbeaton@csumb.edu>
2018-09-19 21:28:58 +0000

    added starting folders and index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Silverjack - Team 4 </title>
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        
+        <?php
+        
+        // include 'inc/functions.php';
+        
+        
+        ?>
+        
+
+    </body>
+</html>

b27e9fc38a78083ad75cc91eca3ef3a5e201868f
Mytchell Beaton <mbeaton@csumb.edu>
2018-09-19 22:52:26 +0000

    added draw hand function

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
-<?php
-    
-    // include 'functions/functions.php';
-    
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
         <title> Silverjack - Team 4 </title>
         <link href="css/styles.css" rel="stylesheet" />
+        
+        <?php
+    
+            include 'functions/function1.php';
+            
+            getHand();
+            echo "<br>";
+            getHand();
+            echo "<br>";
+            getHand();
+            echo "<br>";
+            getHand();
+            
+            
+            
+        ?>
     </head>
     <body>
         

d3bc7a60d6224eb47688e61d9a25e3e9971b3d36
Alexis Oneill <aoneill@csumb.edu>
2018-09-26 05:14:01 +0000

    HTML and styles

---
+++ homepage.php
@@ -0,0 +1,64 @@
+<?php
+    
+    // include 'functions/functions.php';
+    
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Silverjack - Team 4 </title>
+        <link href="css/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="silverjack">
+            <header>Silverjack</header>
+            <br>
+            <div class="p1">
+                <img src="img/moose.png" alt="Moose">
+                <h3>Player 1:Sophisticated Moose</h3>
+            </div>
+            <div class="p1points">
+                <h3>39</h3><br>
+            </div>
+            <div class="p2">
+                <img src="img/bear.png" alt="Bear">
+                <h3>Player 2:Fancy Bear</h3>
+            </div>
+            <div class="p2points">
+                <h3>40</h3><br>
+            </div>
+            <div class="p3">
+                <img src="img/unicorn.png" alt="Unicorn">
+                <h3>Player 3:Derped Unicorn</h3>
+            </div>
+            <div class="p3points">
+                <h3>41</h3><br>
+            </div>
+            <div class="p4">
+                <img src="img/spaceman.png" alt="Spaceman">
+                <h3>Player 4:Cool Spaceman</h3>
+            </div>
+            <div class="p4points">
+                <h3>42</h3><br>
+            </div>
+            <br>
+            <div class="points">
+                <h1>Player 4 wins 69 points!</h1>
+                <h3>Elapsed time:</h3>
+                <h3>Matches played:</h3>
+                <h3>Average elapsed time:</h3>
+                <br>
+                <form id form> 
+                    <input type="submit" name="play" class="button" value="Play Again">
+                </form>
+            </div>
+        </div>
+        <footer>
+            <hr>CST 336:Internet Programming. 2018&copy; O'Neill Beaton Lopez Gieg<br>
+        <strong>Disclaimer:</strong>The information in this webpage is totally fictuous and not true.<br>
+        It is used for academic purposes.<br>
+        <img id="logo"src="https://www.gannett-cdn.com/-mm-/27b51b61ea4b998e2d58764f833d0fa9c82c0bed/c=0-75-600-525/local/-/media/2016/03/26/Salinas/B9321500095Z.1_20160326013639_000_GMLDSI43O.1-0.png.jpg?width=534&height=401&fit=crop">
+        </footer>
+    </body>
+</html>
